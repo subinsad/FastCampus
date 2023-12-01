db9f80dfdb487df12f882c2bf7c49fdc834e3fb8
subbi <92204014+subinsad@users.noreply.github.com>
2023-12-01 18:24:35 +0900

    호이스팅

---
+++ javascript/main.js
@@ -0,0 +1,26 @@
+// 데이터타입확인
+function checkType(data) {
+    return Object.prototype.toString.call(data).slice(8, -1);
+}
+
+//전개 연산자
+function fn(x, y, z) {
+    console.log(x, y, z);
+}
+
+fn(1, 2, 3);
+
+const a = [1, 2, 3];
+fn(...a);
+
+//함수
+
+//함수선언문
+function hello() {}
+
+//호이스팅
+function hello() {
+    console.log('hello');
+}
+
+hello();
